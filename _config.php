85c61e578ad0b21df3df1d81fde2821ea9b1b0d6
romalouis@gmail.com <romalouis@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2012-09-24 05:19:43 +0000

    post query + member dod
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_unleashed/trunk@3372 950b781c-c544-efab-2f99-90083ee69683

--- _config.php
+++ _config.php
@@ -2,4 +2,5 @@
 
 //===================--------------- START ecommerce_unleashed MODULE ----------------==================
 //UnleashedObject::set_api_settings('dfdfdfffdf', 'fdffdfdfdfwfd');
+//Object::add_extension('Member', 'UnleashedMemberDOD');
 //===================---------------- END ecommerce_unleashed MODULE ----------------===================
--- code/UnleashedObject.php
+++ code/UnleashedObject.php
@@ -19,11 +19,66 @@ class UnleashedObject extends Object {
 		}
 	}
 
-	static function post($class) {
-
+	static function get($class, $filters = null, $uID = null) {
+		return self::query('GET', $class, $uID, $filters);
 	}
 
-	static function get($class, $filter = '') {
+	static function post($class, $values = null, $uID = null) {
+		return self::query('POST', $class, $uID, $values);
+	}
+
+	protected static function query($type, $class, $uID = null, $values = null) {
+		if($type != 'GET' && $type != 'POST') return;
+
+		$segment = $class;
+		if($uID) {
+			$segment .= "/$uID";
+		}
+
+		$params = '';
+		if($type == 'GET' && is_array($values)) {
+			$params = http_build_query($values);
+		}
+
+		$signature = base64_encode(hash_hmac('sha256', $params, self::$api_key, true));
+
+		if($params) {
+			$params = "?$params";
+		}
+
+		$format = 'application/' . self::$format;
+
+		$headers = array(
+			"Content-Type: $format",
+			"Accept: $format",
+			"api-auth-id: " . self::$api_id,
+			"api-auth-signature: $signature"
+		);
+
+		try { 
+			$curl = curl_init("https://api.unleashedsoftware.com/$segment$params"); 
+			curl_setopt($curl, CURLINFO_HEADER_OUT, true);
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0); 
+			curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($curl, CURLOPT_TIMEOUT, 20);
+			if($type == 'POST') {
+				curl_setopt($curl, CURLOPT_POST, true);
+				curl_setopt($curl, CURLOPT_POSTFIELDS, $values);
+			}
+			$result = curl_exec($curl); 
+			error_log($result); 
+			curl_close($curl);
+			$function = self::$format . '2array';
+			$result = Convert::$function($result);
+			return $result['Items'];
+		}
+		catch(Exception $e) { 
+			error_log("Unleashed Error: $e"); 
+		}
+	}
+
+	/*static function get($class, $filter = '') {
 		$params = '';
 		if($filter && is_array($filter)) {
 			$params = http_build_query($filter);
@@ -61,5 +116,5 @@ class UnleashedObject extends Object {
 		catch(Exception $e) { 
 			error_log("Unleashed Error: $e"); 
 		}
-	}
+	}*/
 }
