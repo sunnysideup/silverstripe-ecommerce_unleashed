cee76cd259356a2681941caac14ee900dfacc745
romalouis@gmail.com <romalouis@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2012-09-28 05:19:34 +0000

    Error email content modif
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_unleashed/trunk@3403 950b781c-c544-efab-2f99-90083ee69683

--- code/decorator/UnleashedObjectDOD.php
+++ code/decorator/UnleashedObjectDOD.php
@@ -11,7 +11,7 @@ abstract class UnleashedObjectDOD extends DataObjectDecorator {
 	static $errors = array(
 		'U_OBJECT_DELETED' => array('Unleashed Object Not Found', "The Unleashed object had been created but can not be found anymore."),
 		'U_OBJECT_DUPLICATE' => array('Unleashed Object Already Created', "An Unleashed object with the same 'unique field' has been found.\nTherefore, this SS object can not create a new Unleashed object."),
-		'SS_FIELD_MISSING' => array('SS $ClassName #$ID $Field Missing', "The SS \$ClassName #\$ID does not have a '\$Field' value set which is required in order to create a new Unleashed object.")
+		'SS_FIELD_MISSING' => array('SS $ClassName #$ID $FieldName Missing', "The SS \$ClassName #\$ID does not have a '\$FieldName' value set which is required in order to create a new Unleashed object.")
 	);
 
 	function extraStatics() {
@@ -115,7 +115,7 @@ abstract class UnleashedObjectDOD extends DataObjectDecorator {
 		list($subject, $body) = $errors[$type];
 		
 		if($field) {
-			$this->owner->Field = $field;
+			$this->owner->FieldName = $field;
 		}
 		$data = array($this->owner);
 
