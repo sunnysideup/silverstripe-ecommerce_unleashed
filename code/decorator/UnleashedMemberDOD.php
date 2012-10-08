4e3d526b2a1541203ccea6e4c565410a34667747
romalouis@gmail.com <romalouis@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2012-10-08 02:44:15 +0000

    previous class removal
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_unleashed/trunk@3437 950b781c-c544-efab-2f99-90083ee69683

--- code/decorator/UnleashedMemberDOD.php
+++ code/decorator/UnleashedMemberDOD.php
@@ -25,31 +25,3 @@ class UnleashedMemberDOD extends UnleashedObjectDOD {
 		);
 	}
 }
-
-/*class UnleashedMemberDOD extends DataObjectDecorator {
-	
-	static $uclass = 'Customers';
-
-	function onAfterWrite() {
-		$this->updateUDatabase();
-	}
-
-	function updateUDatabase() {
-		$members = UnleashedObject::get(self::$uclass, array('customerCode' => $this->owner->ID));
-		$uID = null;
-		foreach($members as $member) {
-			if($member['CustomerCode'] == $this->owner->ID) {
-				$uID = $member['Guid'];
-			}
-		}
-		$fields = array(
-			'CustomerName' => $this->owner->getName(),
-			'Email' => $this->owner->Email,
-			'Notes' => $this->owner->Notes
-		);
-		if(! $uID) {
-			$fields['CustomerCode'] = $this->owner->ID;
-		}
-		UnleashedObject::post(self::$uclass, $fields, $uID);
-	}
-}*/
