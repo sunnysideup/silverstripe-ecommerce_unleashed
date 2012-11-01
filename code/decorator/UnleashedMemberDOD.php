68f2220fc1ed60be476db6b75d26a3e916c7fa2d
romalouis@gmail.com <romalouis@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2012-11-01 22:10:45 +0000

    submission log date instead of order date
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_unleashed/trunk@3575 950b781c-c544-efab-2f99-90083ee69683

--- code/decorator/UnleashedMemberDOD.php
+++ code/decorator/UnleashedMemberDOD.php
@@ -13,7 +13,7 @@ class UnleashedMemberDOD extends UnleashedObjectDOD {
 		if($sync && $orders) {
 			$name = $this->owner->Surname; // Company Name
 			if(empty($name)) {
-				return $this->notifyError('SS_FIELDS_MISSING', 'Name');
+				return $this->notifyError('SS_FIELDS_MISSING', 'Company Name');
 			}
 			return true;
 		}
