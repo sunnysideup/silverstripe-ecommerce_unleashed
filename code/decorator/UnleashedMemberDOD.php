bf65fd00543695ebdc034538b32cbf114ca37e42
helpfulrobot <chris@silverstripe.com>
2016-01-01 04:41:04 +1300

    Converted to PSR-2

--- code/decorator/UnleashedMemberDOD.php
+++ code/decorator/UnleashedMemberDOD.php
@@ -1,33 +1,37 @@
-<?
+<?php
 
-class UnleashedMemberDOD extends UnleashedObjectDOD {
+class UnleashedMemberDOD extends UnleashedObjectDOD
+{
 	
-	static $u_class = 'Customers';
-	static $unique_fields = array('CustomerCode', 'ID');
+    public static $u_class = 'Customers';
+    public static $unique_fields = array('CustomerCode', 'ID');
 	
-	static $update_after_write = false;
+    public static $update_after_write = false;
 	
-	function synchroniseUDatabase() {
-		$sync = parent::synchroniseUDatabase();
-		$orders = DataObject::get('Order', "MemberID = {$this->owner->ID}"); // $this->owner->Orders() does not work. Has EcommerceRole been added properly ?
-		if($sync && $orders) {
-			$name = $this->owner->Surname; // Company Name
-			if(empty($name)) {
-				return $this->notifyError('SS_FIELDS_MISSING', 'Company Name');
+    public function synchroniseUDatabase()
+    {
+        $sync = parent::synchroniseUDatabase();
+        $orders = DataObject::get('Order', "MemberID = {$this->owner->ID}"); // $this->owner->Orders() does not work. Has EcommerceRole been added properly ?
+		if ($sync && $orders) {
+		    $name = $this->owner->Surname; // Company Name
+			if (empty($name)) {
+			    return $this->notifyError('SS_FIELDS_MISSING', 'Company Name');
 			}
-			return true;
+		    return true;
 		}
-	}
+    }
 
-	function getUFields() {
-		return array(
+    public function getUFields()
+    {
+        return array(
 			'CustomerName' => $this->owner->Surname, // Company Name
 			'Email' => $this->owner->Email,
 			'Notes' => $this->owner->Notes
 		);
-	}
+    }
 
-	function getUFieldsForOrder() {
-		return array('Guid' => $this->owner->GUID);
-	}
+    public function getUFieldsForOrder()
+    {
+        return array('Guid' => $this->owner->GUID);
+    }
 }
--- code/decorator/UnleashedOrderDOD.php
+++ code/decorator/UnleashedOrderDOD.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 /**
  * Customer is required (You can specify only the GUID or CustomerCode and the Customer has to already exist)
@@ -21,97 +21,100 @@
  *
  * OrderStatus is always required Add/Update
  */
-class UnleashedOrderDOD extends UnleashedObjectDOD {
+class UnleashedOrderDOD extends UnleashedObjectDOD
+{
 	
-	static $u_class = 'SalesInvoices';
-	static $unique_fields = array('OrderNumber', 'ID');
+    public static $u_class = 'SalesInvoices';
+    public static $unique_fields = array('OrderNumber', 'ID');
 
-	static $post_format = 'xml';
+    public static $post_format = 'xml';
 
-	static $u_tax_guid;
-	static $attribute_tax_class;
-	static $exclude_attribute_classes = array();
+    public static $u_tax_guid;
+    public static $attribute_tax_class;
+    public static $exclude_attribute_classes = array();
 
-	protected function onAfterWriteStart() {
-		if($this->owner->IsPaid() && ! $this->owner->GUID) {
-			parent::onAfterWriteStart();
-		}
-	}
+    protected function onAfterWriteStart()
+    {
+        if ($this->owner->IsPaid() && ! $this->owner->GUID) {
+            parent::onAfterWriteStart();
+        }
+    }
 
-	function synchroniseUDatabase() {
-		$sync = parent::synchroniseUDatabase();
-		if($sync) {
-			/*$status = $this->owner->getCustomerStatus(false);
+    public function synchroniseUDatabase()
+    {
+        $sync = parent::synchroniseUDatabase();
+        if ($sync) {
+            /*$status = $this->owner->getCustomerStatus(false);
 			if(empty($status)) {
 				return $this->notifyError('SS_FIELDS_MISSING', 'Status');
 			}*/
 
 			$member = $this->owner->Member();
-			if($member->exists()) {
-				$sync = $member->synchroniseUDatabase();
-				if($sync) {
-					$sync = $member->updateUDatabase();
-				}
-				if(! $sync) {
-					return $this->notifyError('SS_RELATION_INVALID', 'Member');
-				}
-			}
-			else {
-				return $this->notifyError('SS_FIELDS_MISSING', 'Member');
-			}
+            if ($member->exists()) {
+                $sync = $member->synchroniseUDatabase();
+                if ($sync) {
+                    $sync = $member->updateUDatabase();
+                }
+                if (! $sync) {
+                    return $this->notifyError('SS_RELATION_INVALID', 'Member');
+                }
+            } else {
+                return $this->notifyError('SS_FIELDS_MISSING', 'Member');
+            }
 
 			// Currency is not required to POST but it should be
 			$currency = $this->getUCurrency();
-			if(empty($currency)) {
-				return $this->notifyError('SS_FIELDS_MISSING', 'Currency');
-			}
+            if (empty($currency)) {
+                return $this->notifyError('SS_FIELDS_MISSING', 'Currency');
+            }
 
-			$tax = $this->getUTax();
-			if(! $tax) {
-				return $this->notifyError('SS_FIELDS_MISSING', 'Unleashed Tax');
-			}
+            $tax = $this->getUTax();
+            if (! $tax) {
+                return $this->notifyError('SS_FIELDS_MISSING', 'Unleashed Tax');
+            }
 
-			$attributes = $this->owner->Attributes();
-			foreach($attributes as $attribute) {
-				if($attribute->ClassName != self::$attribute_tax_class) {
-					if($attribute->CalculatedTotal != 0 || ! in_array($attribute->ClassName, self::$exclude_attribute_classes)) { // Only exclude them if CalculatedTotal equals 0
+            $attributes = $this->owner->Attributes();
+            foreach ($attributes as $attribute) {
+                if ($attribute->ClassName != self::$attribute_tax_class) {
+                    if ($attribute->CalculatedTotal != 0 || ! in_array($attribute->ClassName, self::$exclude_attribute_classes)) { // Only exclude them if CalculatedTotal equals 0
 						// Precondition : Only order items
 						$buyable = $attribute->Buyable(true);
-						$extensions = array_keys($buyable->getExtensionInstances());
-						foreach($extensions as $extension) {
-							if(is_subclass_of($extension, 'UnleashedObjectDOD')) {
-								$sync = $buyable->synchroniseUDatabase();
-								if($sync) {
-									$sync = $buyable->updateUDatabase();
-								}
-								if(! $sync) {
-									return $this->notifyError('SS_RELATION_INVALID', "$buyable->ClassName #$buyable->ID");
-								}
-								break;
-							}
-						}
+                        $extensions = array_keys($buyable->getExtensionInstances());
+                        foreach ($extensions as $extension) {
+                            if (is_subclass_of($extension, 'UnleashedObjectDOD')) {
+                                $sync = $buyable->synchroniseUDatabase();
+                                if ($sync) {
+                                    $sync = $buyable->updateUDatabase();
+                                }
+                                if (! $sync) {
+                                    return $this->notifyError('SS_RELATION_INVALID', "$buyable->ClassName #$buyable->ID");
+                                }
+                                break;
+                            }
+                        }
 						// Todo : deal with other modifiers like delivery
-					}
-				}
-			}
+                    }
+                }
+            }
 
-			return true;
-		}
-	}
+            return true;
+        }
+    }
 
 	/**
 	 * Code generated for XML
      */
-	function getUFields() {
-		$order = $this->owner;
-		$tax = $this->getUTax();
-		$customerRef = $order->Modifiers('OrderMarker');
-		if($customerRef) {
-			$customerRef = $customerRef->First();
-			$customerRef = $customerRef->OrderFor;
-		}
-		$submissionLog = $order->SubmissionLog();
-		$fields = array(
+	public function getUFields()
+	{
+	    $order = $this->owner;
+	    $tax = $this->getUTax();
+	    $customerRef = $order->Modifiers('OrderMarker');
+	    if ($customerRef) {
+	        $customerRef = $customerRef->First();
+	        $customerRef = $customerRef->OrderFor;
+	    }
+	    $submissionLog = $order->SubmissionLog();
+	    $fields = array(
 			'OrderDate' => str_replace(' ', 'T', $submissionLog->Created), // XSD format
 			// QuoteExpiryDate
 			// RequiredDate
@@ -140,95 +143,97 @@ class UnleashedOrderDOD extends UnleashedObjectDOD {
 			// SalesOrderLines
 			// LastModifiedOn
 		);
-		if($order->CanHaveShippingAddress()) {
-			$address = $order->ShippingAddress();
-			$prefix = 'Shipping';
-			if(! $address->exists()) {
-				$address = $order->BillingAddress();
-				$prefix = '';
-			}
-			$fields['DeliveryName'] = implode(' ', array($address->{"{$prefix}Prefix"}, $address->{"{$prefix}FirstName"}, $address->{"{$prefix}Surname"}));
-			$fields['DeliveryStreetAddress'] = $address->{"{$prefix}Address"};
-			$fields['DeliverySuburb'] = $address->{"{$prefix}Address2"};
-			$fields['DeliveryCity'] = $address->{"{$prefix}City"};
-			$fields['DeliveryCountry'] = $address->{"get{$prefix}FullCountryName"}();
-			$fields['DeliveryPostCode'] = $address->{"{$prefix}PostalCode"};
-		}
-		unset($tax);
-		if(self::$attribute_tax_class) { // We suppose it's GSTTaxModifier
+	    if ($order->CanHaveShippingAddress()) {
+	        $address = $order->ShippingAddress();
+	        $prefix = 'Shipping';
+	        if (! $address->exists()) {
+	            $address = $order->BillingAddress();
+	            $prefix = '';
+	        }
+	        $fields['DeliveryName'] = implode(' ', array($address->{"{$prefix}Prefix"}, $address->{"{$prefix}FirstName"}, $address->{"{$prefix}Surname"}));
+	        $fields['DeliveryStreetAddress'] = $address->{"{$prefix}Address"};
+	        $fields['DeliverySuburb'] = $address->{"{$prefix}Address2"};
+	        $fields['DeliveryCity'] = $address->{"{$prefix}City"};
+	        $fields['DeliveryCountry'] = $address->{"get{$prefix}FullCountryName"}();
+	        $fields['DeliveryPostCode'] = $address->{"{$prefix}PostalCode"};
+	    }
+	    unset($tax);
+	    if (self::$attribute_tax_class) { // We suppose it's GSTTaxModifier
 			$tax = $order->Modifiers(self::$attribute_tax_class);
-			if($tax) {
-				$tax = $tax->First();
-				$fields['TaxTotal'] = $tax->CalculatedTotal;
-			}
-		}
-		$subTotal = $taxTotal = 0;
-		$lines = array();
-		$attributes = $this->owner->Attributes();
-		foreach($attributes as $attribute) {
-			if($attribute->ClassName != self::$attribute_tax_class) {
-				if($attribute->CalculatedTotal != 0 || ! in_array($attribute->ClassName, self::$exclude_attribute_classes)) { // Only exclude them if CalculatedTotal equals 0
+	        if ($tax) {
+	            $tax = $tax->First();
+	            $fields['TaxTotal'] = $tax->CalculatedTotal;
+	        }
+	    }
+	    $subTotal = $taxTotal = 0;
+	    $lines = array();
+	    $attributes = $this->owner->Attributes();
+	    foreach ($attributes as $attribute) {
+	        if ($attribute->ClassName != self::$attribute_tax_class) {
+	            if ($attribute->CalculatedTotal != 0 || ! in_array($attribute->ClassName, self::$exclude_attribute_classes)) { // Only exclude them if CalculatedTotal equals 0
 					// We suppose that there are only order items
 					$buyable = $attribute->Buyable(true);
-					$attributeFields = array(
+	                $attributeFields = array(
 						'LineNumber' => count($lines) + 1,
 						'Product' => array('Guid' => $buyable->GUID),
 						'OrderQuantity' => $attribute->Quantity,
 						'UnitPrice' => $attribute->UnitPrice,
 						'LineTotal' => $attribute->Total()
 					);
-					if(isset($tax)) { // We suppose it's GSTTaxModifier
+	                if (isset($tax)) { // We suppose it's GSTTaxModifier
 						$attributeFields['TaxRate'] = $tax->CurrentRate;
-						if($tax->TaxType == 'Inclusive') {
-							foreach(array('UnitPrice', 'LineTotal') as $name) {
-								$attributeFields[$name] = $attributeFields[$name] / (1 + $tax->CurrentRate);
-							}
-						}
-						$attributeFields['LineTax'] = $attributeFields['LineTotal'] * $tax->CurrentRate;
-						$taxTotal += $attributeFields['LineTax'];
-					}
-					$subTotal += $attributeFields['LineTotal'];
-					$lines[] = $attributeFields;
+	                    if ($tax->TaxType == 'Inclusive') {
+	                        foreach (array('UnitPrice', 'LineTotal') as $name) {
+	                            $attributeFields[$name] = $attributeFields[$name] / (1 + $tax->CurrentRate);
+	                        }
+	                    }
+	                    $attributeFields['LineTax'] = $attributeFields['LineTotal'] * $tax->CurrentRate;
+	                    $taxTotal += $attributeFields['LineTax'];
+	                }
+	                $subTotal += $attributeFields['LineTotal'];
+	                $lines[] = $attributeFields;
 					// Todo : deal with other modifiers like delivery
-				}
-			}
-		}
-		$fields['SalesOrderLines']['SalesInvoiceLine'] = $lines;
-		$fields['SubTotal'] = $subTotal;
-		if(isset($tax)) {
-			$fields['TaxTotal'] = $taxTotal;
-			if(bccomp($tax->CalculatedTotal, round($taxTotal, 2)) !== 0) {
-				$errors[] = 'Tax';
-			}
-		}
-		$fields['Total'] = $subTotal + $taxTotal;
-		if(bccomp($order->Total, round($fields['Total'], 2)) !== 0) {
-			$errors[] = 'Total';
-		}
-		if(isset($errors)) {
-			return $this->notifyError('CALCULATION_INCORRECT', $errors);
-		}
-		return $fields;
+	            }
+	        }
+	    }
+	    $fields['SalesOrderLines']['SalesInvoiceLine'] = $lines;
+	    $fields['SubTotal'] = $subTotal;
+	    if (isset($tax)) {
+	        $fields['TaxTotal'] = $taxTotal;
+	        if (bccomp($tax->CalculatedTotal, round($taxTotal, 2)) !== 0) {
+	            $errors[] = 'Tax';
+	        }
+	    }
+	    $fields['Total'] = $subTotal + $taxTotal;
+	    if (bccomp($order->Total, round($fields['Total'], 2)) !== 0) {
+	        $errors[] = 'Total';
+	    }
+	    if (isset($errors)) {
+	        return $this->notifyError('CALCULATION_INCORRECT', $errors);
+	    }
+	    return $fields;
 	}
 
-	function getUCurrency() {
-		$currency = $this->owner->CurrencyUsed();
-		if($currency->exists() && ! empty($currency->Code)) {
-			return $currency->Code;
-		}
-		return Payment::site_currency();
-	}
+    public function getUCurrency()
+    {
+        $currency = $this->owner->CurrencyUsed();
+        if ($currency->exists() && ! empty($currency->Code)) {
+            return $currency->Code;
+        }
+        return Payment::site_currency();
+    }
 
-	function getUTax() {
-		if(self::$u_tax_guid) {
-			$taxes = UnleashedAPI::get('Taxes');
-			if($taxes) {
-				foreach($taxes as $tax) {
-					if($tax['Guid'] == self::$u_tax_guid) {
-						return $tax;
-					}
-				}
-			}
-		}
-	}
+    public function getUTax()
+    {
+        if (self::$u_tax_guid) {
+            $taxes = UnleashedAPI::get('Taxes');
+            if ($taxes) {
+                foreach ($taxes as $tax) {
+                    if ($tax['Guid'] == self::$u_tax_guid) {
+                        return $tax;
+                    }
+                }
+            }
+        }
+    }
 }
