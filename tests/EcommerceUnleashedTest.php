83e20e6270f9de86f3983f7a1e74802f6ec3201d
sunnysideup <sunnysideupdevs@gmail.com>
2017-01-03 14:12:46 +1300

    PATCH: module clean-up

---
+++ tests/EcommerceUnleashedTest.php
@@ -0,0 +1,13 @@
+<?php
+class EcommerceUnleashedTest extends SapphireTest {
+
+	protected $usesDatabase = false;
+
+	protected $requiredExtensions = array();
+
+	public function testMyMethod() {
+		$this->assertEquals(1, 1);
+	}
+
+}
+
